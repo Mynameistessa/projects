916d605876af6924c95b14ec909af07b2a7b85e1
Mynameistessa <tessabreen@protonmail.com>
2023-12-15 16:43:40 +0000

    Add banner component

---
+++ components/banner.tsx
@@ -0,0 +1,15 @@
+"use client"
+import '../app/globals.css'
+
+export default function Banner() {
+  return (
+        <header className='sticky top-0'>
+          <div className="w-full bg-cover bg-center" 
+          style={{ height:'8rem', 
+          backgroundImage: 'url(/Banner-test.png)'}}> 
+          </div>
+        </header>
+  )
+}
+
+
