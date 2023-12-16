d9ad2c40776a71a255df4dad377cee3e8a1efca1
Mynameistessa <tessabreen@protonmail.com>
2023-12-16 19:25:05 +0000

    Simplify banner

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,13 +1,10 @@
 "use client"
-import '../app/globals.css'
 
 export default function Banner() {
   return (
         <header className='sticky top-0'>
-          <div className="w-full bg-cover bg-center" 
-          style={{ height:'8rem', 
-          backgroundImage: 'url(/Banner-test.png)'}}> 
-          </div>
+          <div className="w-full bg-cover bg-center h-[8rem]" 
+          style={{backgroundImage: 'url(/Banner-test.png)'}} />
         </header>
   )
 }
